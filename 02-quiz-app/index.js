4bdebb1c339b73230611057bfb40c9ebd67e05b4
AbhishekMM27 <abhishek.agrawal@mutualmobile.com>
2021-02-06 23:58:23 +0530

    completed quiz app

--- 02-quiz-app/index.js
+++ 02-quiz-app/index.js
@@ -1,6 +1,6 @@
 const quizData = [
   {
-    question: 'What is the most used programming language in 2019?',
+    question: 'What is the most used programming language in 2021?',
     a: 'Java',
     b: 'C',
     c: 'Python',
@@ -19,7 +19,7 @@ const quizData = [
     question: 'What does HTML stand for?',
     a: 'Hypertext Markup Language',
     b: 'Cascading Style Sheet',
-    c: 'Jason Object Notation',
+    c: 'Javascript Object Notation',
     d: 'Helicopters Terminals Motorboats Lamborginis',
     correct: 'a',
   },
@@ -32,3 +32,64 @@ const quizData = [
     correct: 'b',
   },
 ];
+
+const question = document.getElementById('question');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+const answersEls = document.querySelectorAll('.answer');
+const submitBtn = document.getElementById('submit');
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+  deselectAnswers();
+  const currentQuestion = quizData[currentQuestionIndex];
+  question.innerText = currentQuestion.question;
+  a_text.innerText = currentQuestion.a;
+  b_text.innerText = currentQuestion.b;
+  c_text.innerText = currentQuestion.c;
+  d_text.innerText = currentQuestion.d;
+}
+
+function getSelectedAnswer() {
+  let ansID = undefined;
+  answersEls.forEach((ans) => {
+    if (ans.checked) {
+      ansID = ans.id;
+    }
+  });
+  return ansID;
+}
+
+function deselectAnswers() {
+  answersEls.forEach((ans) => {
+    ans.checked = false;
+  });
+}
+
+submitBtn.addEventListener('click', () => {
+  const selectedAnsIndex = getSelectedAnswer();
+  if (selectedAnsIndex) {
+    if (quizData[currentQuestionIndex].correct === selectedAnsIndex) {
+      score++;
+    }
+    currentQuestionIndex++;
+    if (currentQuestionIndex < quizData.length) {
+      loadQuiz();
+    } else {
+      document.getElementById('quiz').innerHTML = `
+        <h2>You are finished with score of ${score} / ${quizData.length} ${
+        score === 4 ? '💪🏻' : '👍🏻'
+      }</h2>
+        <button onclick="location.reload()">Reload Quiz</button>
+      `;
+    }
+  } else {
+    alert('Please select an option to continue');
+  }
+});
